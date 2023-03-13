36310a81b19b90bf39882ac2e8f729a8ecad19b8
Faisal Khan <faisaldev404@gmail.com>
2023-03-13 10:38:29 +0500

    setting up the project

---
+++ index.js
@@ -0,0 +1,7 @@
+let inputValue = document.getElementById("inputValue")
+let saveBtn = document.getElementById("saveButton")
+
+
+saveBtn.addEventListener("click", () => {
+    console.log("clicked");
+})
